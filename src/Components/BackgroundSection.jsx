2e8bb0faeca67f08d6ae15d1ffddaeead94f6de4
kapaintsil <kwame.bixcuit@gmail.com>
2025-08-18 09:31:38 +0000

    fixed typing errors

--- src/Components/BackgroundSection.jsx
+++ src/Components/BackgroundSection.jsx
@@ -13,13 +13,13 @@ const BackgroundSection = () => (
     </div>
     <div className="introduction">
       <p>
-        Embarking on your programming journey? Look no further. CodeNex is your compass in the realm of programming languages. We're dedicated to laying down the solid foundation you need to understand, embrace, and excel in the world of coding.
+        Embarking on your programming journey? Look no further. TechPath is your compass in the realm of programming languages. We're dedicated to laying down the solid foundation you need to understand, embrace, and excel in the world of coding.
       </p>
       <p>
-        At CodeNex, we believe that every coder begins with a single step. Our user-friendly platform offers crisp and concise introductions to a variety of programming languages, demystifying the intricacies for beginners. Whether you're intrigued by Python's elegance, JavaScript's versatility, or C++'s power, we've got you covered.
+        At TechPath, we believe that every coder begins with a single step. Our user-friendly platform offers crisp and concise introductions to a variety of programming languages, demystifying the intricacies for beginners. Whether you're intrigued by Python's elegance, JavaScript's versatility, or C++'s power, we've got you covered.
       </p>
       <p>
-        But why stop at an introduction? CodeNex takes you beyond the basics. We're your launchpad to an array of handpicked websites and resources that facilitate deeper exploration. These carefully curated materials are your trusted companions on the journey from novice to ninja.
+        But why stop at an introduction? TechPath takes you beyond the basics. We're your launchpad to an array of handpicked websites and resources that facilitate deeper exploration. These carefully curated materials are your trusted companions on the journey from novice to ninja.
       </p>
     </div>
   </div>
